d58d3b954271f59b7598c10ca1eec62ebce736b7
blackhat-hemsworth <114207848+blackhat-hemsworth@users.noreply.github.com>
2024-04-19 16:02:09 -0500

    data gotten, initial pass at vis

--- get-results.py
+++ get-results.py
@@ -2,15 +2,21 @@ import pandas as pd
 import requests
 import bs4 
 from io import StringIO
+import time
 
+table_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
 url = 'https://fbref.com/en/comps/9/passing/Premier-League-Stats'
 base_url = 'https://fbref.com'
 
+html = requests.get(table_url)
+bs_obj = bs4.BeautifulSoup(html.text,"lxml")
+for table in bs_obj.findAll("table"):
+    df_table = pd.read_html(StringIO(str(table)),extract_links ="all")[0].apply(lambda col: [base_url + v[1] if v[1] else v[0] for v in  col])
+    df_table.columns = [tt[0] or tt[1] for tt in df_table.columns]
+    df_table.to_csv("table.csv")
+    break
+
 html = requests.get(url)
-with open('checking.html', 'wb+') as f:
-    f.write(html.content)
-
-
 bs_obj = bs4.BeautifulSoup(html.text,"lxml")
 i = 0
 for comment in bs_obj.findAll(string=lambda string:isinstance(string, bs4.Comment)):
@@ -18,44 +24,29 @@ for comment in bs_obj.findAll(string=lambda string:isinstance(string, bs4.Commen
     for table in c_soup.findAll("table"):
         df = pd.read_html(StringIO(str(table)),extract_links ="all")[0].apply(lambda col: [base_url + v[1] if v[1] else v[0] for v in  col])
         i+=1
+        df.columns = [tt[1][0] or tt[1][1] or tt[0][0] or tt[0][1] for tt in df.columns]
+        df = df[df.Rk != 'Rk']
         df.to_csv("df"+str(i)+".csv")
 
-tables = bs_obj.findAll("table") 
-for table in tables:
-    df = pd.read_html(StringIO(str(table)))[0]
-    i+=1
-    df.to_csv("df"+str(i)+".csv")
-
-
-
-quit()
-
-bs_obj = bs4.BeautifulSoup(html.text,"lxml")
-tables = bs_obj.findAll('table') 
-for table in tables:
-    df = pd.read_html(StringIO(str(table)))
-    print(df)
-
-
-quit()
+df = df_table.merge(df, how = 'left', on = 'Squad')
 
 list_of_dfs = list()
-for n in range(1,68):
-    for ab in ['A','B']:
-        url = f'https://electionresults.sos.mn.gov/results/Index?ErsElectionId=162&scenario=StateRepresentativePnp&DistrictCode={n}{ab}&show=Go'
-        print(url)
-        html = requests.get(url)
-        bs_obj = bs4.BeautifulSoup(html.text,"lxml")
-        tables = bs_obj.findAll('table') 
-        for table in tables:
-            df = pd.read_html(StringIO(str(table)))
-            if len(df) != 0 and 'Democratic-Farmer-Labor' in df[0].columns: 
-                df[0]["District"] = str(n)+ab
-                list_of_dfs.append(df[0]) 
-
-print(list_of_dfs)
+for match_log_url in df.Matches:
+    time.sleep(5)
+    print("Getting " + match_log_url)
+    html = requests.get(match_log_url)
+    bs_obj = bs4.BeautifulSoup(html.text,"lxml")
+    for table in bs_obj.findAll("table"):
+        df_temp = pd.read_html(StringIO(str(table)),extract_links ="all")[0].apply(lambda col: [base_url + v[1] if v[1] else v[0] for v in  col])
+        df_temp.columns = [tt[1][0] or tt[1][1] or tt[0][0] or tt[0][1] for tt in df_temp.columns]
+        df_temp = df_temp[df_temp.Day != ""]
+        df_temp["source"] = match_log_url
+        print(df_temp.shape)
+        list_of_dfs.append(df_temp)
 
 main_df = pd.concat(list_of_dfs)
-print(main_df)
+main_df.to_csv("main_df.csv")
 
-main_df.to_csv("election-results.csv")
+j_df = df.merge(main_df, how = 'left', left_on = 'Matches', right_on = 'source')
+
+j_df.to_csv("joined_df.csv")
